1abd70662df6e23485c61a9128ced7cba88e0799
mengze <1305856104@qq.com>
2022-08-10 16:03:00 +0800

    初始化数据库配置

--- src/main/java/net/lz1998/pbbot/SpringMiraiServerApplication.java
+++ src/main/java/net/lz1998/pbbot/SpringMiraiServerApplication.java
@@ -1,11 +1,13 @@
 package net.lz1998.pbbot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@MapperScan("net.lz1998.pbbot.dao")
 public class SpringMiraiServerApplication {
 
     public static void main(String[] args) {
